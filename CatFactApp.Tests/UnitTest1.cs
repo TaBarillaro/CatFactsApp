b457841899c01bf9e44ba32c6aad03c05614b937
TaniaBar <tania.barillaro1@gmail.com>
2025-08-05 14:41:15 +0200

    Création projet de test pour les tests unitaires et interface ICatFactService

---
+++ CatFactApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CatFactApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CatFactsApp/Services/ICatFactService.cs
@@ -0,0 +1,14 @@
+﻿using CatFactsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatFactsApp.Services
+{
+    internal interface ICatFactService
+    {
+        Task<CatFact> GetCatFactAsync();
+    }
+}
